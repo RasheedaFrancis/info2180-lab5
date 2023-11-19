14abccb1bef32c0f27e59eb1c8039e256083ee8b
Rasheeda Francis <rasheedafrancis02@gmail.com>
2023-11-19 17:44:22 -0500

    Execise 5 completed-

--- world.js
+++ world.js
@@ -3,13 +3,46 @@ window.onload = function () {
         event.preventDefault();
 
         let country = document.getElementById("country").value.trim().replace(/(<([^>]+)>)/gi, "");
+        
+        
+        
 
         fetch(`http://localhost/info2180-lab5/world.php?country=${country}`)
             .then(response => {
+               
                 return response.text();
             })
             .then(data => {
+                if (country === "") {
+                    document.getElementById("result").innerHTML = 'Please enter a valid country name.</p>';
+                }else{
                 document.getElementById("result").innerHTML = data;
+                }
+            })
+            .catch(error => {
+                console.error('Error fetching data:', error.message);
+            });
+    });
+    document.getElementById("lookup_cities").addEventListener("click", function (event) {
+        event.preventDefault();
+
+        let country = document.getElementById("country").value.trim().replace(/(<([^>]+)>)/gi, "");
+
+        fetch(`http://localhost/info2180-lab5/world.php?country=${country}&lookup=cities`)
+            .then(response => {
+                
+               
+                return response.text();
+                
+               
+            })
+            .then(data => {
+               
+                if (country === "") {
+                    document.getElementById("result").innerHTML = 'Please enter a valid country name.</p>';
+                }else{
+                document.getElementById("result").innerHTML = data;
+                }
             })
             .catch(error => {
                 console.error('Error fetching data:', error.message);
--- world.php
+++ world.php
@@ -11,26 +11,56 @@ $stmt = $conn->query("SELECT * FROM countries");
 
 if(isset($_GET['country'])){
   $country = filter_var($_GET['country'], FILTER_SANITIZE_STRING);
-  $stmt = $conn->query("SELECT * FROM countries WHERE name like'%$country%'");
+  
+  if (isset($_GET['lookup']) && $_GET['lookup'] === 'cities') {
+    
+    $stmt = $conn->query("SELECT cities.name, cities.district, cities.population FROM cities JOIN countries ON cities.country_code = countries.code  WHERE countries.name LIKE '%$country%'");
+} else {
+  
+    $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+}
+
 }
 
 
+
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+if (empty($results)) {
+  echo "No data found for the specified country: $country";
+  exit;
+}
 
 ?>
-<table >
-    <tr>
-        <th>Country Name</th>
-        <th>Continent</th>
-        <th>Independence Year</th>
-        <th>Head of State</th>
-    </tr>
-    <?php foreach ($results as $row): ?>
+
+<table>
+    <?php if (isset($_GET['lookup']) && $_GET['lookup'] === 'cities' ): ?>
         <tr>
-            <td><?= $row['name']; ?></td>
-            <td><?= $row['continent']; ?></td>
-            <td><?= $row['independence_year']; ?></td>
-            <td><?= $row['head_of_state']; ?></td>
+            <th>Name</th>
+            <th>District</th>
+            <th>Population</th>
         </tr>
-    <?php endforeach; ?>
+        <?php foreach ($results as $row): ?>
+            <tr>
+                <td><?= $row['name']; ?></td>
+                <td><?= $row['district']; ?></td>
+                <td><?= $row['population']; ?></td>
+            </tr>
+        <?php endforeach; ?>
+    <?php else: ?>
+        <tr>
+            <th>Country Name</th>
+            <th>Continent</th>
+            <th>Independence Year</th>
+            <th>Head of State</th>
+        </tr>
+        <?php foreach ($results as $row): ?>
+            <tr>
+                <td><?= $row['name']; ?></td>
+                <td><?= $row['continent']; ?></td>
+                <td><?= $row['independence_year']; ?></td>
+                <td><?= $row['head_of_state']; ?></td>
+            </tr>
+        <?php endforeach; ?>
+    <?php endif; ?>
 </table>
+ 
