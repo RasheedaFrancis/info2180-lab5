c4f1a29c744621c276f51a1deb9a6bba498c4a7e
Rasheeda Francis <rasheedafrancis02@gmail.com>
2023-11-19 15:34:52 -0500

    Execise 4 completed-

--- world.php
+++ world.php
@@ -1,4 +1,5 @@
 <?php
+header("Access-Control-Allow-Origin: *");
 
 $host = 'localhost';
 $username = 'lab5_user';
@@ -17,8 +18,19 @@ if(isset($_GET['country'])){
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
-<ul>
-<?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
-<?php endforeach; ?>
-</ul>
+<table >
+    <tr>
+        <th>Country Name</th>
+        <th>Continent</th>
+        <th>Independence Year</th>
+        <th>Head of State</th>
+    </tr>
+    <?php foreach ($results as $row): ?>
+        <tr>
+            <td><?= $row['name']; ?></td>
+            <td><?= $row['continent']; ?></td>
+            <td><?= $row['independence_year']; ?></td>
+            <td><?= $row['head_of_state']; ?></td>
+        </tr>
+    <?php endforeach; ?>
+</table>
